f254d27467b799ae98ed95efa0b17f99d36ea408
Aria Stewart <aredridel@dinhe.net>
2016-01-25 17:00:00 -0600

    Refactor with named functions

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 'use strict'
 
 const http = require('http');
-const Router = require('router');
-const P = require('bluebird');
-
 P.promisifyAll(http.Server.prototype);
 
+const Router = require('router');
+
+const P = require('bluebird');
+
 const router = Router();
+
+const server = http.createServer((req, res) => router(req, res, () => done(req, res)));
+
+server.listenAsync(process.env.PORT || 3880).then(() => logPort(server), logErrorAndExit);
+
+function logPort(server) {
+    console.log("listening on port", server.address().port);
+}
+
+function logErrorAndExit(err) {
+    console.warn(err);
+    process.exit(1);
+}
 
 function done(req, res) {
     res.statusCode = 404;
     res.end('not found\n');
 }
-
-const server = http.createServer((req, res) => router(req, res, () => done(req, res)));
-
-server.listenAsync(process.env.PORT || 3880).then(x => console.log("listening on port", server.address().port)).catch(err => {
-    console.warn(err);
-    process.exit(1);
-});
